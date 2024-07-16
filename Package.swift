33a60384773c0c7c6c4b0141669a26f088e6c327
iOS Foundations <no-reply@google.com>
2024-07-16 11:24:13 -0700

    feat: Release version 0.2.0
    PiperOrigin-RevId: 652916259

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ import PackageDescription
 let package = Package(
   name: "GooglePlacesSwift", platforms: [.iOS(.v15)],
   products: [.library(name: "GooglePlacesSwift", targets: ["PlacesSwift"])],
-  dependencies: [.package(url: "https://github.com/googlemaps/ios-places-sdk", from: "9.0.0")],
+  dependencies: [.package(url: "https://github.com/googlemaps/ios-places-sdk", from: "9.1.0")],
   targets: [
     .binaryTarget(
       name: "GooglePlacesSwift",
-      url: "https://dl.google.com/geosdk/swiftpm/0.1.0/google_places_swift.xcframework.zip",
+      url: "https://dl.google.com/geosdk/swiftpm/0.2.0/google_places_swift.xcframework.zip",
       checksum: "cc0858f9bbf8ae16273519c5fedf6cd83ad8284c531739e273dd7f19ce528bf7"
     ),
     .target(
